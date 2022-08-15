8e8bee3be90dafabbaa6531f657e84fb238f3ec8
Jeffords Camferdam <97536686+rcam2022@users.noreply.github.com>
2022-08-15 09:19:36 -0500

    Create 1.1.9 Personal Timeline.java

---
+++ 1.1.9 Personal Timeline.java
@@ -0,0 +1,38 @@
+public class Timeline
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        
+        System.out.println("Age       | M/YY - Description");
+            System.out.println("7         | 10/1 - I went somewhere");
+                System.out.println("          |");
+                     System.out.println("          |");
+                         System.out.println("          |");
+                            System.out.println("9         |  ");
+                                 System.out.println("          |");
+                                      System.out.println("          | 2/10 - I went to the beach");
+                                         System.out.println("          |");
+                                          System.out.println("11        | 7/11 - I went to my house ");
+                                         System.out.println("          |");
+                                         System.out.println("          |");
+                                         System.out.println("          |");
+                                      System.out.println("13        |");
+                                       System.out.println("          | 9/13 - Started job");
+                                 System.out.println("          |");
+                                 System.out.println("          |  11/14 - Won tournament");
+                                 System.out.println("15        |");
+                                         System.out.println("          |  8/15 - Started school");
+                                            System.out.println("          |");
+                                            System.out.println("          |");
+                                             System.out.println("17        |");
+
+
+
+
+
+
+
+
+    }
+}
