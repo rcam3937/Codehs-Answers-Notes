c4be73966657d9e288b42bf541bd16fe31afed24
Jeffords Camferdam <97536686+rcam2022@users.noreply.github.com>
2022-08-15 09:23:24 -0500

    Create 1.3.6 Weight of a Pyramid.java

---
+++ 1.3.6 Weight of a Pyramid.java
@@ -0,0 +1,11 @@
+public class Pyramid 
+{
+    public static void main(String[] args)
+    {
+        int numBlocks = 2500000;
+        double blockWeight = 2.5;
+        
+        System.out.print("The pyramid weighs ");
+        System.out.println(numBlocks*blockWeight);
+    }
+}
