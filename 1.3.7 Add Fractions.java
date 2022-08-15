88d9363ea444fd2e76d2ca64276854e485c610f3
Jeffords Camferdam <97536686+rcam2022@users.noreply.github.com>
2022-08-15 09:24:05 -0500

    Create 1.3.7 Add Fractions.java

---
+++ 1.3.7 Add Fractions.java
@@ -0,0 +1,30 @@
+public class AddFractions 
+{
+    public static void main(String[] args)
+    {
+        //Your code goes here! 
+        
+        int fractionOnenumerator = 1;
+        int fractionOnedenominator = 4;
+        int fractionTwonumerator = 2;
+        int fractionTwodenominator = 3;
+        
+        System.out.print("The numerator of the first fraction is ");
+        System.out.println(fractionOnenumerator);
+        System.out.print("The denominator of the first fraction is ");
+        System.out.println(fractionOnedenominator);
+        System.out.print("The numerator of the second fraction is ");
+        System.out.println(fractionTwonumerator);
+        System.out.print("The denominator of the second fraction is ");
+        System.out.println(fractionTwodenominator);
+        System.out.print("The sum of 1/4 + 2/3 = 11/12");
+        
+
+        
+        
+
+        
+        
+        
+            }
+}
