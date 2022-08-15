e578136ffbdc0e4b061b289f699597316c3525f3
Jeffords Camferdam <97536686+rcam2022@users.noreply.github.com>
2022-08-15 09:24:49 -0500

    Create 1.4.5 Work Shift.java

---
+++ 1.4.5 Work Shift.java
@@ -0,0 +1,19 @@
+public class WorkShift
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        int num = 60;
+        
+        int numHours = 20;
+        int minutes = 42;
+        int seconds = 16;
+        
+        numHours *= num*60;
+        minutes *= num;
+        
+        System.out.print(numHours+seconds+minutes);
+        
+        
+    }
+}
